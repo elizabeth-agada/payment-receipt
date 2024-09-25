ad676ae43c3faba57f63d4362869fdcb5ee1e582
elizabeth-agada <elizadave6@gmail.com>
2024-09-25 16:23:26 +0100

    fixing the paper fit

--- src/components/PaymentDetails.tsx
+++ src/components/PaymentDetails.tsx
@@ -40,23 +40,23 @@ const PaymentDetails: React.FC<PaymentDetailsProps> = ({
 }) => {
   return (
     <>
-      <h2 className='text-2xl font-semibold'>SHF</h2>
-      <h2 className='text-2xl font-semibold'>Single Haulage Fee</h2>
-      <h2 className=''>{date} {time}</h2>
-      <h2 className="">======================</h2>
-      <h2 className=''>payer: {payerId}</h2>
-      <h2 className=''>Phone Number: {phoneNumber}</h2>
-      <h2 className=''>Vehicle Number: {vehicleNumber}</h2>
-      <h2 className=' w-60'>Vehicle Type: {vehicleType}</h2>
-      <h2 className=''>Origin State: {originState}</h2>
-      <h2 className=''>Origin LGA: {originLga}</h2>
-      <h2 className=''>Destination State: {destinationState}</h2>
-      <h2 className=''>Destination LGA: {destinationLga}</h2>
-      <h2 className=''>Contact: {contact}</h2>
-      <h2 className='w-48 '>Transaction Reference: {transactionReference}</h2>
-      <h2 className=''>Terminal ID: {terminalId}</h2>
-      <h2 className=''>Date: {date} {time}</h2>
-      <h2 className="">=====================</h2>
+      <h2 className='text-xl font-semibold'>SHF</h2>
+      <h2 className='text-xl font-semibold'>Single Haulage Fee</h2>
+      <h2 className='text-lg'>{date} {time}</h2>
+      <h2 className="text-lg">======================</h2>
+      <h2 className='text-lg'>payer: {payerId}</h2>
+      <h2 className='text-lg'>Phone Number: {phoneNumber}</h2>
+      <h2 className='text-lg'>Vehicle Number: {vehicleNumber}</h2>
+      <h2 className='text-lg w-60'>Vehicle Type: {vehicleType}</h2>
+      <h2 className='text-lg'>Origin State: {originState}</h2>
+      <h2 className='text-lg'>Origin LGA: {originLga}</h2>
+      <h2 className='text-lg'>Destination State: {destinationState}</h2>
+      <h2 className='text-lg'>Destination LGA: {destinationLga}</h2>
+      <h2 className='text-lg'>Contact: {contact}</h2>
+      <h2 className='w-48 text-lg'>Transaction Reference: {transactionReference}</h2>
+      <h2 className='text-lg'>Terminal ID: {terminalId}</h2>
+      <h2 className='text-lg'>Date: {date} {time}</h2>
+      <h2 className="text-lg">=====================</h2>
       <h2 className='font-semibold text-2xl'>Amount Paid: {amountPaid}</h2>
       <h2 className='font-semibold text-2xl'>Status: {status}</h2>
     </>
