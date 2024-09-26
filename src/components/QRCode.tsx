4f7b2049119f12153633efd5e3d50e6bded5a61d
elizabeth-agada <elizadave6@gmail.com>
2024-09-26 14:31:53 +0100

    i updated the payment details page

--- src/components/QRCode.tsx
+++ src/components/QRCode.tsx
@@ -6,7 +6,7 @@ interface QRCodeProps {
   size?: number;  // Optional size prop
 }
 
-const QRCodeComponent: React.FC<QRCodeProps> = ({ data, size = 200 }) => {
+const QRCodeComponent: React.FC<QRCodeProps> = ({ data, size = 100 }) => {
   return (
     <div className=''>
       <QRCode value={data} size={size} className='px-2'/>
